f3b67eb0c37912c46676b085ba48d4bc16c7862a
Peter Oettig <peet1993@gmail.com>
2020-04-11 15:57:20 +0200

    Fixed csproj file.

--- compile.sh
+++ compile.sh
@@ -6,9 +6,24 @@ error() {
 		exit 1
 	fi
 
-	echo -e "\033[1;33m$1\033[0m" 2>&1
+	echo -e "\033[1;31m$1\033[0m" 2>&1
 	exit 1
 }
+
+# Recompile the bot
+echo -e "\033[1;33mRecompiling the bot...\033[0m"
+if ! [ -d TS3AudioBot ]; then
+	git clone --recursive https://github.com/jwiesler/TS3AudioBot.git || error "Failed to clone repository."
+fi
+cd TS3AudioBot || error "Failed to cd into the bot repository."
+git pull || error "Failed to pull repository."
+sed -i -e '/GitVersionTask/,+3d' TS3AudioBot/TS3AudioBot.csproj # Fix bug in master with GitVersionTask
+dotnet build --framework netcoreapp3.1 --configuration Release TS3AudioBot || error "Compilation of TS3AudioBot failed."
+rsync -a --progress TS3AudioBot/bin/Release/netcoreapp3.1/ ../../ || error "RSync failed."
+cd ..
+rm -rf TS3AudioBot
+echo -e "\033[1;33mFinished building the bot!\033[0m"
+echo "---------------------------------------------------"
 
 for plugin in */; do
 	plugin=${plugin%/}
