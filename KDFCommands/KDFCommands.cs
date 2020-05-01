2385150cf3c6fd62b1bceec78c64ccc30d6c3a10
Peter Oettig <peet1993@gmail.com>
2020-05-01 15:07:05 +0200

    Fixed GetNameFromUid when the Uid is anonymous.

--- KDFCommands/KDFCommands.cs
+++ KDFCommands/KDFCommands.cs
@@ -279,6 +279,11 @@ public class KDFCommands : IBotPlugin {
 	}
 
 	private static string GetName(PlayQueue queue, int index, TsFullClient ts3FullClient) {
+		Console.WriteLine(queue.Items[index].MetaData.ResourceOwnerUid?.Value);
+		if (queue.Items[index].MetaData.ResourceOwnerUid?.Value == "Anonymous") {
+			return  GetClientNameFromUid(ts3FullClient, ts3FullClient.Identity.ClientUid);
+		}
+
 		return GetClientNameFromUid(ts3FullClient, queue.Items[index].MetaData.ResourceOwnerUid);
 	}
 
